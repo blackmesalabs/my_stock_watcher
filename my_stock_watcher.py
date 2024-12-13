1059f61537e973c08c6e661613673bafc713c4f1
Kevin Hubbard <kevinhub@comcast.net>
2024-12-13 08:44:54 -0800

    -history plotting added
    Plot 5 year history when -history flag presented.  Also -loop flag for looping every 15 minutes.

--- my_stock_watcher.py
+++ my_stock_watcher.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2024 Kevin M. Hubbard Black Mesa Labs
 # Source file: my_stock_watcher.py
-# Date:    12.11.24
+# Date:    12.13.24
 # Author:  Kevin M. Hubbard
 # Description: Given a list of stock symbols, use yfinance to look up how the
 #              the stock is doing today. Any movement above 1% is highlighted
@@ -24,7 +24,8 @@
 # pip install yfinance
 #
 # If SSL Failure, do the following per:
-#   https://stackoverflow.com/questions/78166871/ssl-certificate-verify-failed-yahoo-finance-api-python
+#   https://stackoverflow.com/questions/78166871/
+#         ssl-certificate-verify-failed-yahoo-finance-api-python
 # Modify data.py in c:\python\Lib\site-packages\yfinance\data.py
 #  to add "verify=False" to web call
 #
@@ -34,14 +35,24 @@
 #           proxies=proxy,
 #           timeout=timeout,
 #           allow_redirects=True, verify=False)
+# 
+# Example usage:
+#  %python my_stock_watcher.py ^GSPC AMD AMZN MSFT NVDA
+#  %python my_stock_watcher.py stock_list.txt
+#  %python my_stock_watcher.py stock_list.txt -loop
+#  %python my_stock_watcher.py NFLX -history
 #
-# stock_list.txt is a clear text file of stock symbols that looks like this:
+# Note: stock_list.txt is a clear text file of stock symbols that looks like this:
 # [stock_list.txt]
-# AMD
-# INTC
-# NVDA
-
-import sys,platform,os,time;
+#  AMD
+#  INTC
+#  NVDA
+# History:
+# 2024.12.11 : Created
+# 2024.12.12 : Added 52wk 10% low,high color highlighting
+# 2024.12.13 : Added history plotting
+#####################################################################################
+import sys,platform,os,time,datetime;
 import yfinance as yf
 
 def main():
@@ -62,38 +73,67 @@ def main():
          };
 
   args = sys.argv + [None]*4;# args[0] is "foo.py"
-# stock_list = file2list( args[1] );
-  stock_list = file2list( "stock_list.txt" );
-
-  loop_mode = False;
-  for each in sys.argv:
+ 
+  stock_list = []; 
+  loop_mode    = False;
+  history_mode = False;
+  for each in sys.argv[1:]:
+    if "-history" in each:
+      history_mode = True;
     if "-loop" in each:
       loop_mode = True;
+      loop_interval = 15;# Minutes between updates
+  for each in sys.argv[1:]:
     if "-" not in each:
-      symbol = each;
-      stock_info = yf.Ticker( symbol );
-      print( stock_info.info );
-      return;
-
+      if os.path.exists( each ):
+        stock_list = file2list( each );
+      else:
+        stock_list += [ each ];
 
   loop_forever = True;
   while loop_forever:
+    print( time.ctime() );
     for each_stock in stock_list:
       words = " ".join(each_stock.split()).split(' ') + [None] * 4;
       symbol = words[0];
+      if history_mode:
+        plot_history(ansi, symbol );
       stock_info = yf.Ticker( symbol );
       print( display_line(ansi, stock_info ) );
     loop_forever = loop_mode;
-    time.sleep( 15*60 );
+    if loop_forever:
+      time.sleep( loop_interval*60 );
   return;
+
+def plot_history( ansi, stock_symbol ):
+  # period: data period to download (either use period parameter or use start and end) 
+  #   “1d”, “5d”, “1mo”, “3mo”, “6mo”, “1y”, “2y”, “5y”, “10y”, “ytd”, “max”
+  # interval: data interval:
+  #   “1m”, “2m”, “5m”, “15m”, “30m”, “60m”, “90m”, “1h”, “1d”, “5d”, “1wk”, “1mo”, “3mo”
+  # start: If not using period – in the format (yyyy-mm-dd) or datetime.
+  # end: If not using period – in the format (yyyy-mm-dd) or datetime.
+  df = yf.download( stock_symbol, period='5y', interval="1mo")
+  price_list = [];
+  for i in range(0,len(df) ):
+    price_list += [ ( df.iloc[i].name, df.iloc[i]["Close"].iloc[0] ) ];
+  max_value = max(price_list, key=lambda item: item[1])[1];
+  scale = max_value / 70;
+  for (each_date, each_price) in price_list:
+    txt = str(each_date).split(' ')[0] + ":";
+    txt += " "*int( each_price / scale );
+    txt = txt + "$%d" % int( each_price );
+    print(txt);
+  return
 
 def display_line( ansi, stock_info ):
   symbol     = stock_info.info["symbol"];
 # name       = stock_info.info["longName"];
   if stock_info.info.get("currentPrice"):
     price_new  = stock_info.info["currentPrice"];
-  else:
+  elif stock_info.info.get("bid"):
     price_new  = stock_info.info["bid"];# ^GSPC doesn't have currentPrice
+  else:
+    price_new = 0;
 
   price_old  = stock_info.info["previousClose"];
   price_low  = stock_info.info["fiftyTwoWeekLow"];
@@ -117,33 +157,32 @@ def display_line( ansi, stock_info ):
     my_color = ansi['dim'];
 
   # Highlight if within 10% of 52wk Low or High
-  low_percent =   ( price_new / price_low  );
-  high_percent =  ( price_new / price_high );
+  low_percent  = ( price_new / price_low  );
+  high_percent = ( price_new / price_high );
 
   w52_h = "";
   w52_l = "";
-  if high_percent > 0.90:
+  if ( high_percent > 0.90 ):
     w52_h = ansi['bold']+ansi['fg_grn'];
   else:
     w52_h = ansi['dim'];
-  if low_percent < 1.10:
+  if ( low_percent < 1.10 ):
     w52_l = ansi['bold']+ansi['fg_red'];
   else:
     w52_l = ansi['dim'];
 
-  str = my_bold;
-  str = str + symbol.ljust(6);
-  str = str + " " + ("%.2f" % price_new).rjust(7);
-# str = str + ansi['dim'];
-  str = str + " [" + w52_l + ("%.0f" % price_low  ).ljust(4) + my_color_reset;
-  str = str + ansi['dim'] + ":" + w52_h + ("%.0f" % price_high ).rjust(4) + my_color_reset + "]";
-  str = str + ansi['reset'];
-  str = str + my_color;
-  str = str + " " + ("%+.2f" % price_delta).rjust(6);
-  str = str + " " + ("%+2.1f%%" % change).rjust(6);
-  str = str + my_color_reset;
-# str = str + "%f %f" % ( low_percent, high_percent );
-  return str;
+  txt = my_bold;
+  txt = txt + symbol.ljust(6);
+  txt = txt + " " + ("%.2f" % price_new).rjust(7);
+  txt = txt + my_color_reset;
+  txt = txt + " [" + w52_l + ("%.0f" % price_low  ).ljust(4) + my_color_reset;
+  txt = txt + ansi['dim'] + ":" + w52_h + ("%.0f" % price_high ).rjust(4) + my_color_reset + "]";
+  txt = txt + ansi['reset'];
+  txt = txt + my_color;
+  txt = txt + " " + ("%+.2f" % price_delta).rjust(6);
+  txt = txt + " " + ("%+2.1f%%" % change).rjust(6);
+  txt = txt + my_color_reset;
+  return txt;
 
 def file2list( file_name ):
   file_in  = open( file_name, 'r' );
